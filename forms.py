2a5c892926b780da5c5a4ab989fb79946721b6dd
jholmes16 <jholmesarea@gmail.com>
2022-03-15 16:52:39 -0400

    create the forms

--- forms.py
+++ forms.py
@@ -1,1 +1,13 @@
 # forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntergerField, SubmitField
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+
+    id = IntergerField("Id Number of Puppy to Remove: ")
+    submit = SubmitField("Remove Puppy")

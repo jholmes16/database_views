e4c5a717705fceb2ca37e100387924e43fa58373
jholmes16 <jholmesarea@gmail.com>
2022-03-16 15:31:53 -0400

    add templates

--- adoption_site.py
+++ adoption_site.py
@@ -1,9 +1,11 @@
 # adoption_site.py
 import os
-from forms import AddForm, DelForm
+from forms import  AddForm , DelForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
@@ -13,8 +15,7 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 ### SQL DATABASE SECTION ###############
 ########################################
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -27,7 +28,7 @@ Migrate(app, db)
 class Puppy(db.Model):
 
     __tablename__ = 'puppies'
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.Text)
 
     def __init__(self,name):
@@ -36,51 +37,51 @@ class Puppy(db.Model):
     def __repr__(self):
         return f"Puppy name: {self.name}"
 
-###############################################
-######### VIEW FUNCTIONS -- HAVE FORMS ########
-###############################################
+############################################
 
+        # VIEWS WITH FORMS
+
+##########################################
 @app.route('/')
 def index():
     return render_template('home.html')
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pup():
-
     form = AddForm()
 
     if form.validate_on_submit():
-
         name = form.name.data
 
+        # Add new Puppy to database
         new_pup = Puppy(name)
         db.session.add(new_pup)
         db.session.commit()
 
         return redirect(url_for('list_pup'))
 
-    return render_template('add.html', form=form)
+    return render_template('add.html',form=form)
 
 @app.route('/list')
 def list_pup():
-
+    # Grab a list of puppies from database.
     puppies = Puppy.query.all()
     return render_template('list.html', puppies=puppies)
 
-@app.route('/delete', methods=['GET', 'Post'])
+@app.route('/delete', methods=['GET', 'POST'])
 def del_pup():
 
     form = DelForm()
 
     if form.validate_on_submit():
-
         id = form.id.data
         pup = Puppy.query.get(id)
         db.session.delete(pup)
-        de.session.commit()
+        db.session.commit()
 
         return redirect(url_for('list_pup'))
-    return render_template('delet.html', form=form)
+    return render_template('delete.html',form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
-# forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntergerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField
+
+
 
 class AddForm(FlaskForm):
 
-    name = StringField('Name of Puppy: ')
+    name = StringField('Name of Puppy:')
     submit = SubmitField('Add Puppy')
 
 class DelForm(FlaskForm):
 
-    id = IntergerField("Id Number of Puppy to Remove: ")
-    submit = SubmitField("Remove Puppy")
+    id = IntegerField('Id Number of Puppy to Remove:')
+    submit = SubmitField('Remove Puppy')
